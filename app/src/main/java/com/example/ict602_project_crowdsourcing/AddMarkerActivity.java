148618abe2b5bbe10c6f17adbce19ba955001bb4
Nasghul01 <nasrul01arsyad@gmail.com>
2023-01-10 17:54:41 +0800

    add marker

---
+++ app/src/main/java/com/example/ict602_project_crowdsourcing/AddMarkerActivity.java
@@ -0,0 +1,14 @@
+package com.example.ict602_project_crowdsourcing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMarkerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_marker);
+    }
+}
